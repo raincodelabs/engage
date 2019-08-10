1a46bbbb9e3d4e3772eacc6891340a529a121422
Vadim Zaytsev <vadim@grammarware.net>
2019-08-10 22:28:30 +0200

    extend the grammar to cover a realistic part of the language

--- Engage/front/Parser.g
+++ Engage/front/Parser.g
@@ -1,10 +1,38 @@
 ﻿[namespace Engage.front]
 [class public Parser]
+[using Engage.mid]
+
+EngSpec ::= i:s?
+	"types" Types=TypeDecl*,(i:s*)
+	"tokens" Tokens=TokenDecl*,(i:s*)
+	"handlers" Handlers=HandlerDecl*,(i:s*) i:s?;
+
+TypeDecl ::= Names=Id+,"," ("<:" i:s? Super=Id)?;
+
+TokenDecl ::= Names=Lexeme+,"," "::" i:s? Type=Id;
+
+Lexeme ::=
+	  LiteralLex := Literal=Quoted  Special=$false
+	/ NumberLex  := "number" Special=$true
+	/ StringLex  := "string" Special=$true;
+
+HandlerDecl ::= LHS=Trigger "->" RHS=Reaction;
+
+Trigger ::= (Literal=Quoted / "EOF" EOF=$true) ("given" Flag=Id)?;
+
+Reaction ::=
+	  PushReaction := "push" Name=Id ("(" Args=Id+,"," ")")?
+	/ LiftReaction := "lift" Flag=Id
+	/ DropReaction := "lift" Flag=Id
+	;
+
+string Id ::= re:"[a-zA-Z_01-9]+";
+string Quoted ::= "'" re:"[\\w]+" "'";
 
 # Spacing
 string s ::= (i:Lay/i:comment)+;
 string Lay ::= LayNoNL / NL;
 # The first three spaces are 0xA0 representations, then it's a space, then a tab
 string LayNoNL ::= rei:"[\uFFFD\u00A0  \t]";
-string comment ::= "//" re:"[^\r\n]*" ;
+string comment ::= "%" re:"[^\r\n]*" ;
 string NL ::= ("\r\n" / "\n" / "\r");
---
+++ Engage/mid/AST.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace Engage.mid
+{
+    public partial class EngSpec
+    {
+        public List<TypeDecl> Types = new List<TypeDecl>();
+        public List<TokenDecl> Tokens = new List<TokenDecl>();
+        public List<HandlerDecl> Handlers= new List<HandlerDecl>();
+    }
+
+    public partial class TypeDecl
+    {
+        public List<string> Names = new List<string>();
+        public string Super = "";
+    }
+
+    public partial class TokenDecl
+    {
+        public List<Lexeme> Names = new List<Lexeme>();
+        public string Type;
+    }
+
+    public partial class Lexeme
+    {
+        public bool Special;
+    }
+
+    public partial class LiteralLex : Lexeme
+    {
+        public string Literal;
+    }
+
+    public partial class NumberLex : Lexeme
+    {
+    }
+
+    public partial class StringLex : Lexeme
+    {
+    }
+
+    public partial class HandlerDecl
+    {
+        public Trigger LHS;
+        public Reaction RHS;
+    }
+
+    public partial class Trigger
+    {
+        public string Literal = "";
+        public bool EOF = false;
+        public string Flag = "";
+    }
+
+    public partial class Reaction
+    {
+    }
+
+    public partial class PushReaction : Reaction
+    {
+        public string Name;
+        public List<string> Args = new List<string>();
+    }
+
+    public partial class LiftReaction : Reaction
+    {
+        public string Flag;
+    }
+
+    public partial class DropReaction : Reaction
+    {
+        public string Flag;
+    }
+
+    public partial class Context
+    {
+        public List<Assignment> Lets = new List<Assignment>();
+    }
+
+    public partial class Assignment
+    {
+    }
+}
