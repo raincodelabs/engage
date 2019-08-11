24721d98d580a1411dad849b8ac55b254622d8b5
Vadim Zaytsev <vadim@grammarware.net>
2019-08-12 01:03:13 +0200

    establish a runtime for the sake of simplification

---
+++ EngageRuntime/BaseParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EngageRuntime
+{
+    public class BaseParser
+    {
+        protected string input;
+        protected int pos;
+
+        protected List<Message> Pending = new List<Message>();
+        private Stack<object> Main = new Stack<object>();
+
+        public BaseParser(string _input)
+        {
+            input = _input;
+            pos = 0;
+        }
+
+        protected void Log(string message)
+            => Console.WriteLine("[LOG] " + message);
+
+        protected void Push(object _x)
+        {
+            Main.Push(_x);
+            Trigger();
+        }
+
+        protected void Schedule(Type _type, Func<object, int> _action)
+        {
+            Message message = new Message(_type, _action);
+            Pending.Add(message);
+            Trigger();
+        }
+
+        private void Trigger()
+        {
+            while (Main.Count > 0)
+                if (ApplyOne(Main.Peek()))
+                    Main.Pop(); // discard and continue
+                else
+                    return; // all currently pending actions fail at the moment
+        }
+
+        private bool ApplyOne(object _x)
+        {
+            Type _t = _x.GetType();
+            foreach (var candidate in Pending)
+            {
+                if (candidate.IsWanted(_t))
+                {
+                    int code = candidate.Handler(_x);
+                    if (code == Message.Misfire)
+                        continue;
+                    if (code == Message.Perfect)
+                        Pending.Remove(candidate);
+                    return true; // Consume or Perfect
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ EngageRuntime/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EngageRuntime
+{
+    public class Message
+    {
+        public const int Misfire = 1; // keep looking / did not consume anything
+        public const int Consume = 2; // success / keep me for future
+        public const int Perfect = 3; // termination / remove m
+
+        public System.Type ExpectedType;
+
+        public Func<object, int> Handler;
+
+        public Message(System.Type type, Func<object, int> handler)
+        {
+            ExpectedType = type;
+            Handler = handler;
+        }
+
+        public bool IsWanted(System.Type type)
+            => type == ExpectedType || type.IsSubclassOf(ExpectedType);
+    }
+}
