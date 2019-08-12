47e480d022384128a665933cb244a32251fa3475
Vadim Zaytsev <vadim@grammarware.net>
2019-08-13 01:38:34 +0200

    add a test generator

--- AB/Program.cs
+++ AB/Program.cs
@@ -1,12 +1,27 @@
 using System;
+using System.IO;
 
 namespace AB
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string TestPath = @"..\..\..\..\tests\";
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var gen = new TestGenerator();
+            for (uint i = 0; i < 101; i++)
+                File.WriteAllLines(Path.Combine(TestPath, $"deep{i}.ab"), gen.GenerateNestedIfs(i));
+            Console.WriteLine("100 test programs generated");
+            for (uint i = 0; i < 5; i++)
+                File.WriteAllLines(Path.Combine(TestPath, $"deep10e{i}.ab"), gen.GenerateNestedIfs((ulong)Math.Pow(10, i)));
+            Console.WriteLine("5 more test programs generated");
+            for (uint i = 0; i < 101; i++)
+                File.WriteAllLines(Path.Combine(TestPath, $"long{i}.ab"), gen.GenerateFlatVersatile(i));
+            Console.WriteLine("100 more test programs generated");
+            for (uint i = 0; i < 8; i++)
+                File.WriteAllLines(Path.Combine(TestPath, $"long10e{i}.ab"), gen.GenerateFlatVersatile((ulong)Math.Pow(10, i)));
+            Console.WriteLine("8 more test programs generated");
         }
     }
 }
---
+++ AB/TestGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AB
+{
+    internal class TestGenerator
+    {
+        private readonly Random r = new Random();
+
+        internal List<string> GenerateNestedIfs(ulong n)
+        {
+            List<string> lines = new List<string>();
+            for (ulong i = 0; i < n; i++)
+                lines.Add(new string(' ', (int)i) + $"if X{i}");
+            lines.Add("RETURN");
+            for (ulong i = n; i > 0; i--)
+                lines.Add(new string(' ', (int)i) + "endif");
+            return lines;
+        }
+
+        internal List<string> GenerateFlatVersatile(ulong n)
+        {
+            List<string> lines = new List<string>();
+            for (ulong i = 0; i < n; i++)
+                lines.Add(RandomStmt());
+            return lines;
+        }
+
+        internal string RandomStmt()
+        {
+            switch (r.Next(0, 5))
+            {
+                case 0:
+                    return RandomIf();
+
+                case 1:
+                    return RandomClear();
+
+                case 2:
+                    return RandomMap();
+
+                default:
+                    return "return";
+            };
+        }
+
+        internal string RandomIf()
+            => $"if {RandomExpr()} return endif";
+
+        internal string RandomClear()
+            => $"clear {RandomVar()}";
+
+        internal string RandomMap()
+            => $"map {RandomExpr()} to {RandomVar()}";
+
+        internal string RandomExpr()
+            => r.Next(0, 2) == 0 ? RandomVar() : RandomNum();
+
+        internal string RandomNum()
+            => r.Next(int.MaxValue).ToString();
+
+        internal string RandomVar()
+        {
+            string s = "";
+            for (int i = 0; i < r.Next(1, 20); i++)
+                s += (char)('a' + r.Next(0, 26));
+            return s;
+        }
+    }
+}
